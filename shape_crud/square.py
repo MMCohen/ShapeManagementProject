b314c2c614ce5baa33bf31814971f1dca96b98fd
Mendel <MendelCohen770@gmail.com>
2026-05-27 11:40:59 +0300

    correcting square file function and parmeters

--- shape_crud/square.py
+++ shape_crud/square.py
@@ -5,8 +5,6 @@ class Square(Shape):
     def __init__(self, side, shape_id, shape_type):
         super().__init__(shape_id, shape_type)
         self.side = side
-        self.shape_id = self.get_shape_id()  # todo: add function in Shape class
-        self.type = "square"
 
     def get_area(self):
         return self.side * self.side
