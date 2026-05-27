9630e43d6d9e6d65cd2869653be8d3b18f62ecdc
Mendel <MendelCohen770@gmail.com>
2026-05-27 11:14:18 +0300

    add basic functions and attributes to Square class

--- shape_crud/square.py
+++ shape_crud/square.py
@@ -2,4 +2,14 @@ from shape import Shape
 
 
 class Square(Shape):
-    pass
+    def __init__(self, side, shape_id, shape_type):
+        super().__init__(shape_id, shape_type)
+        self.side = side
+        self.shape_id = self.get_shape_id()  # todo: add function in Shape class
+        self.type = "square"
+
+    def get_area(self):
+        return self.side * self.side
+
+    def get_perimeter(self):
+        return self.side * 4
