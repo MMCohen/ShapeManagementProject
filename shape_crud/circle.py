df8a8016dbf59248b0495561fcbb34dbfd5295ea
Mendel <MendelCohen770@gmail.com>
2026-05-27 11:39:39 +0300

    add circle file with basic functions

---
+++ shape_crud/circle.py
@@ -0,0 +1,13 @@
+from shape import Shape
+
+
+class Circle(Shape):
+    def __init__(self,shape_id, shape_type, radius):
+        super().__init__(shape_id, shape_type)
+        self.radius = radius
+
+    def get_area(self):
+        return 3.1415 * (self.radius ** 2)
+
+    def get_perimeter(self):
+        return 2 * self.radius * 3.1415
