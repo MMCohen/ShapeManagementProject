a80470f007b9de109bb75ca213c23bb133945abd
Mendel <MendelCohen770@gmail.com>
2026-05-27 16:56:49 +0300

    add imports to shape manager file

--- shape_crud/shape_manager.py
+++ shape_crud/shape_manager.py
@@ -1,6 +1,13 @@
+import logging
+
+from square import Square
+from circle import Circle
+from rectangle import Rectangle
+
+
 class ShapeManager:
     def __init__(self):
-        self.shapes = []
+        self.shapes: list[object] = []
         self.load_from_json()
 
     def create_shape(self, shape):
